7b3a5e358fa2ae15b90d2a3b164f9e67e195d43f
Javohir <javohirjon.1992@gmail.com>
2021-05-07 09:20:00 +0500

    Docker tutorial

--- tutorial/mainapp/urls.py
+++ tutorial/mainapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello, name="Hello"),
+    path('', views.hello, name='hello'),
 ]
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,9 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('hello/', include('mainapp.urls')),
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
+    
 ]
